d0a36b47f8635e030d528d6f81e04021b80c6297
Amila Silva <amilamanoj@gmail.com>
2018-02-09 14:57:18 +0100

    dropped jsp, added webjars

--- src/main/java/org/amila/wordanalyzer/Application.java
+++ src/main/java/org/amila/wordanalyzer/Application.java
@@ -1,7 +1,6 @@
 package org.amila.wordanalyzer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,14 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
