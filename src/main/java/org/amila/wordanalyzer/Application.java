751577b2632ffe7c6a0681209a7d947d9b1e4954
Amila Silva <amilamanoj@gmail.com>
2022-10-31 12:14:19 +0100

    version upgrade

--- src/main/java/org/amila/wordanalyzer/Application.java
+++ src/main/java/org/amila/wordanalyzer/Application.java
@@ -1,13 +1,10 @@
 package org.amila.wordanalyzer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan
-@EnableAutoConfiguration
-public class Application extends SpringBootServletInitializer {
+@SpringBootApplication
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
